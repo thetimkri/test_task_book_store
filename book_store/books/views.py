fce4a3e071aa6296ce6273b9cbdb4c2714f4d31d
Timothey Krivunchenko <timofej.k@team.nekidaem.ru>
2024-04-07 13:20:38 +0200

    add Views Register,login, logout

--- book_store/books/views.py
+++ book_store/books/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'books/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return redirect('login')
+    else:
+        form = LoginForm()
+    return render(request, 'books/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
