a2813a1737d354c9555e19c3dbc557cce5945612
Timothey Krivunchenko <timofej.k@team.nekidaem.ru>
2024-04-07 13:10:59 +0200

    add models:Book, Favorite,News

--- book_store/books/models.py
+++ book_store/books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
